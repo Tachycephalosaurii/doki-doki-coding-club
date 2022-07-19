107dcb0e3cb4f20911f73432abd1e65dc57fed98
Tachycephalosaurii <96754559+Tachycephalosaurii@users.noreply.github.com>
2022-07-18 21:20:17 -0700

    moved things

--- Scripts/Displayables.js
+++ Scripts/Displayables.js
@@ -1,37 +1,3 @@
-var pos = (v, type) => {
-  if (!type) {
-    if (Number.isInteger(v)) type = 'int';
-    else if (!Number.isNaN(v)) type = 'float';
-    else type = typeof v;
-  }
-  switch (type) {
-    case 'int': return v + 'px';
-    case 'float': return (v * 100) + '%';
-    case 'string':
-      if (v.indexOf('px') + v.indexOf('%') === -2) {
-        if (!Number.isNaN(+v)) return pos(+v);
-        else return v;
-      } else return v;
-    default: return v;
-  }
-}
-
-var getUnit = (num) => {
-  var u = /[^0-9.,]+/.exec(num);
-  if (u !== null) return u[0];
-}
-var getValue = (num) => +num.replace(/[^0-9.,]+/, '');
-
-var float = (num) => {
-  if (Number.isNaN(+num)) return float(+getValue(num)) + getUnit(num);
-  return +num.toLocaleString("en", { useGrouping: false, minimumFractionDigits: 1 });
-}
-
-var int = (num) => {
-  if (Number.isNaN(+num)) return int(+getValue(num)) + getUnit(num);
-  return Math.round(+num);
-}
-
 class Displayable {
   constructor(src) {
     this.html = document.createElement('img');
